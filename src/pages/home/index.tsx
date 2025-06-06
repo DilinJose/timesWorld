6ce38516b588dc8b3dfca550d31d382576e68bfd
Dilin <dilinjose34@gmail.com>
2025-06-06 16:53:09 +0530

    Home page design

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Countries</h1>
+
+        <div>
+            <h6>All</h6>
+            <h6>Asia</h6>
+            <h6>Europe</h6>
+        </div>
+    </div>
+  )
+}
+
+export default Home
